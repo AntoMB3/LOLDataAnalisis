c5df4b390e88ed96462822a415e88aac3a2e7e1b
AntoMB3 <antonio.munbar3@gmail.com>
2023-04-14 16:58:23 -0600

    Agregar consulta de partidas
    Crear la funcion para tener un listado de partidas dinamico

--- Scoutting/mylibraries.py
+++ Scoutting/mylibraries.py
@@ -1,4 +1,9 @@
 import requests
+import riotwatcher as rtw
+import creds
+from urllib.parse import urljoin
+connection = rtw.LolWatcher(creds.riot_API_Key)
+
 def GetChampsNamesAndMasteries(lista) -> list:
     try:
         url = "https://ddragon.leagueoflegends.com/cdn/13.7.1/data/en_US/champion.json"
@@ -23,3 +28,38 @@ def GetChampsNamesAndMasteries(lista) -> list:
         
         campeones.append(infoCampeon)
     return campeones
+
+def getMatchList(puuid : str, cantidadPartidas: int = 20,queque: int = None, index : int = 0):
+    partidas = []
+    url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid
+    i = 0
+    while(i <= int(cantidadPartidas/100)):
+        if(i <= 0):
+            parametros = "ids?start="+str(index)+"&count="+str(cantidadPartidas)+"&api_key="+creds.riot_API_Key
+            url_completa = url+"/"+parametros
+            response = requests.get(url_completa)
+            partidas = list(response.json())
+        else:
+            new_index = 100*i #0-99
+            if(cantidadPartidas < 100*(i+1)):
+                new_cantidad = 100*(i+1)-cantidadPartidas
+            else:
+                new_cantidad = 100
+            parametros = "ids?start="+str(new_index)+"&count="+str(new_cantidad)+"&api_key="+creds.riot_API_Key
+            url_completa = url+"/"+parametros
+            response = requests.get(url_completa)
+            partidasExtra = response.json()
+            for partidaExtra in partidasExtra:
+                partidas.append(partidaExtra)
+        i += 1
+    
+    print(len(partidas))
+
+    
+def GetMatchInfo(region: str,match_id):
+    region = region.lower()
+    info_partida = connection.match.by_id(region = region, match_id=match_id)
+    return info_partida
+
+
+getMatchList('_ROPUnorv3aVovk2pvxxKOpkipZxHUZJH-yOToXlJLZ7397Ubg0R_NJS3H4rG_RjgpidV1dVBkveOA',500)
